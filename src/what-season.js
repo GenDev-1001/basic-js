038a2b86cbdf752891394ad43fa34c689eec38d0
GenDev-1001 <evgenii.ubileii@bk.ru>
2022-04-27 20:11:16 +0300

    feat: added what season task

--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+    const ERROR = new Error('Invalid date!')
+    if (!date) {
+        return "Unable to determine the time of year!";
+    }
+    if (date instanceof Date && Object.keys(date).length === 0) {
+        switch (date.getMonth()) {
+            case 11:
+            case 0:
+            case 1:
+                return 'winter';
+            case 2:
+            case 3:
+            case 4:
+                return 'spring';
+            case 5:
+            case 6:
+            case 7:
+                return 'summer';
+            case 8:
+            case 9:
+            case 10:
+                return 'autumn';
+        }
+    } else {
+        throw ERROR;
+    }
 }
 
+
 module.exports = {
-  getSeason
+    getSeason
 };
